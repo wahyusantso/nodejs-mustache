4178c8000a0b0d7cfe433bcb2160297b60b59588
wahyusantso <wahyusantozo36@gmail.com>
2025-12-15 22:53:00 +0700

    159: Mustache Cache

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -6,3 +6,10 @@ test('Menggunakan Mustache', () => {
     const data = mustache.render('Hello {{name}}', {name: "Renyui"});
     expect(data).toBe('Hello Renyui');
 });
+
+test('Menggunakan Mustache Cache', () => {
+    mustache.parse('Hello {{name}}'); //compile template terlebih dahulu, untuk mempercepat proses ketika akan di render
+
+    const data = mustache.render('Hello {{name}}', {name: "Renyui"});
+    expect(data).toBe('Hello Renyui');
+});
